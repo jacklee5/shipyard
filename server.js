bb7c8498d7d94f2bcf34b85a824097a00f99b5ae
Jack Lee <jack200336@gmail.com>
2018-08-01 20:02:33 -0400

    some stuff

--- server.js
+++ server.js
@@ -6,13 +6,38 @@ var socketIO = require('socket.io');
 var app = express();
 var server = http.Server(app);
 var io = socketIO(server);
-app.set('port', 5000);
+var bodyParser = require('body-parser');
+var multer = require('multer');
+var upload = multer();
+
+let sqlite3 = require('sqlite3');
+let db = new sqlite3.Database('./db.sqlite');
+
+// for parsing application/json
+app.use(bodyParser.json()); 
+
+// for parsing application/xwww-
+app.use(bodyParser.urlencoded({ extended: true })); 
+//form-urlencoded
+
+// for parsing multipart/form-data
+app.use(upload.array()); 
+
 app.use(express.static('static'));
+app.use(express.static('views'));
 // Routing
 app.get("/", (request, response) => {
-  response.sendFile(__dirname + '/views/index.html')
+    response.sendFile(__dirname + '/views/index.html');
 });
+
+app.get("/create-account", (request, response) => {
+    response.sendFile(__dirname + '/views/createAccount.html');
+});
+app.post("/create-account", (request, response) => {
+    console.log(request.body);
+});
+
 // Starts the server.
 server.listen(3000, function() {
-  console.log('Starting server on port 3000');
+    console.log('Starting server on port 3000');
 });
