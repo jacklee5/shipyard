c374d84469a837bd3d2dfd62e46e27353ad6beca
TheLuckyRobot <rliang2004@gmail.com>
2018-08-07 21:32:55 -0400

    Added title ;) and pages for tabs to click onto.

--- static/js/index.js
+++ static/js/index.js
@@ -2,45 +2,35 @@
 //this code is currently not in use
 /* When the user clicks on the button, toggle between hiding and showing the dropdown content */
 			
-/*var accountOptions = document.getElementById("profile-button");
+var accountOptions = document.getElementById("user-button");
 
-var selectedBool = false;
+var openedBool = false;
 
 function showHide() {
-    document.getElementById("dropdown-menu").classList.toggle("show");
-    //accountOptions.classList.toggle("white");
-    switch(selectedBool) {
-        case false:
-            accountOptions.setAttribute("style", "color: white;");
-            selectedBool = true;
-        break;
-
-        case true:
-            accountOptions.setAttribute("style", "color: #3de1ff;");
-            selectedBool = false;
-        break;
-    }
+	switch (openedBool) {
+		case false: 
+			document.getElementById("dropdown-menu").setAttribute("style", "display: block");
+			openedBool = true;
+		break;
+		
+		case true:
+			document.getElementById("dropdown-menu").setAttribute("style", "display: none");
+			openedBool = false;
+		break;
+	}
 }
 
 accountOptions.addEventListener('click', showHide);
 
 // Close the dropdown if the user clicks outside of it
 window.onclick = function(event) {
-  if (!event.target.matches('.dropdown-button')) {
-
-    accountOptions.setAttribute("style", "color: #3de1ff;");
-    selectedBool = false;
-
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-
-    for (var i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
+  if (!event.target.matches('.user-button')) {
+	
+	
+	document.getElementById("dropdown-menu").setAttribute("style", "display: none");
+	openedBool = false;
   }
-}*/
+}
 
 /* Switch between pages when the user clicks on the respective tabs */
 
